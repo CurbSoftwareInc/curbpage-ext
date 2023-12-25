ea78e47b455925446c9de7673c0875b356eca798
Robert Alexander <curbsoftware@gmail.com>
2023-12-25 08:44:53 -0800

    working prototype

--- background.js
+++ background.js
@@ -1,12 +1,2 @@
-chrome.action.onClicked.addListener((tab) => {
-  // Check if the URL starts with 'chrome://'
-  if (!tab.url || tab.url.startsWith('chrome://')) {
-    console.log("Cannot inject script into a chrome:// URL");
-    return;
-  }
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ['inject.js']
-  });
-});
+// background.js
+// This script is currently not used but can be utilized for future background tasks
--- popup.js
+++ popup.js
@@ -1,174 +1,165 @@
-// popup.js
-let editingUrl = null;
-
-// Setup event listeners immediately
-setupEventListeners();
-loadUrls();
-
-function setupEventListeners() {
-    const saveButton = document.getElementById('saveButton');
-    const newUrlInput = document.getElementById('newUrlInput');
-    const wpButton = document.getElementById('wp-button');
-
-    saveButton?.addEventListener('click', handleAddEditUrl);
-    newUrlInput?.addEventListener('keypress', event => {
-        if (event.key === 'Enter') {
-            handleAddEditUrl();
-        }
-    });
-    wpButton?.addEventListener('click', () => {
-        chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-            chrome.scripting.executeScript({
-                target: {tabId: tabs[0].id},
-                function: toggleWpAdminBar
-            });
-        });
-    });
-}
-
-function loadUrls() {
-    chrome.storage.sync.get(['urls'], function(result) {
-        if (result.urls) {
-            result.urls.forEach(url => addUrlToGrid(url));
-        }
-    });
-}
-
-function handleAddEditUrl() {
-    var newUrl = document.getElementById('newUrlInput').value;
-    if (!newUrl) {
-        alert("Please enter a URL.");
-        return;
-    }
-    if (!newUrl.startsWith('/')) {
-        alert("URL must start with '/'.");
-        return;
-    }
-    if (editingUrl !== null) {
-        updateUrlInGrid(editingUrl, newUrl);
-    } else {
-        addUrlToGrid(newUrl);
-    }
-    saveUrls();
-    document.getElementById('newUrlInput').value = '';
-    editingUrl = null;
-}
-
-function addUrlToGrid(url) {
-    var list = document.getElementById('urlList');
-
-    // Create a new div for the row
-    var rowDiv = document.createElement('div');
-    rowDiv.className = 'grid-row';
-
-    // Navigate button
-    var navigateButton = createGridButton('Go', 'navigateButton', function() {
-        navigateToRelativeUrl(url);
-    });
-    rowDiv.appendChild(navigateButton);
-
-    // URL text
-    var urlDiv = document.createElement('div');
-    urlDiv.textContent = url;
-    urlDiv.className = 'urlEntry';
-    rowDiv.appendChild(urlDiv);
-
-    // Edit button
-    var editButton = createGridButton('Edit', 'editButton', function() {
-        editUrl(url, urlDiv);
-    });
-    rowDiv.appendChild(editButton);
-
-    // Delete button
-    var deleteButton = createGridButton('Delete', 'deleteButton', function() {
-        deleteUrlFromGrid(rowDiv);
-    });
-    rowDiv.appendChild(deleteButton);
-
-    // Append the row to the list
-    list.appendChild(rowDiv);
-}
-
-function createGridButton(text, className, onClick) {
-    var button = document.createElement('button');
-    button.textContent = text;
-    button.classList.add(className, 'button');
-    button.addEventListener('click', onClick);
-    return button;
-}
-
-function editUrl(url, urlDiv) {
-    var navigateButton = urlDiv.parentNode.querySelector('.navigateButton');
-    document.getElementById('newUrlInput').value = url;
-    editingUrl = { url, element: urlDiv, navigateButton };
-}
-
-
-function deleteUrlFromGrid(rowDiv) {
-    rowDiv.remove();
-    saveUrls();
-}
-
-function updateUrlInGrid(oldUrl, newUrl) {
-    if (editingUrl && editingUrl.element) {
-        editingUrl.element.textContent = newUrl;
-        editingUrl.navigateButton.removeEventListener('click', navigateToRelativeUrl);
-        editingUrl.navigateButton.addEventListener('click', function() {
-            navigateToRelativeUrl(newUrl);
-        });
-    }
-    saveUrls();
-}
-
-function navigateToRelativeUrl(url) {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        var currentTab = tabs[0];
-        var newUrl = new URL(currentTab.url);
-        newUrl.pathname = url;
-        chrome.tabs.update(currentTab.id, {url: newUrl.href});
-    });
-}
-
-function saveUrls() {
-    const urls = Array.from(document.querySelectorAll('.urlEntry')).map(entry => entry.textContent);
-    chrome.storage.sync.set({ urls }, function() {
-        console.log('URLs are saved');
-    });
-}
-
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('wp-button').addEventListener('click', function() {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.scripting.executeScript({
-                target: {tabId: tabs[0].id},
-                function: toggleWpAdminBar
-            });
-        });
-    });
+    loadAccordions();
+    document.getElementById('addAccordionForm').addEventListener('submit', handleAddAccordion);
 });
 
-function toggleWpAdminBar() {
-    var wpAdminBar = document.getElementById('wpadminbar');
-    var body = document.body;
-
-    // Check and set initial inline style if not present
-    if (wpAdminBar && wpAdminBar.style.transform === '') {
-        wpAdminBar.style.transform = 'translateY(0px)';
-        body.style.marginTop = '0px';
-    }
-
-    if (wpAdminBar) {
-        wpAdminBar.style.transition = 'transform 0.3s ease-in-out';
-        body.style.transition = 'margin-top 0.3s ease-in-out';
-        if (wpAdminBar.style.transform === 'translateY(0px)') {
-            // Move the admin bar up and adjust body margin
-            wpAdminBar.style.transform = 'translateY(-32px)';
-            body.style.marginTop = '-32px';
-        } else {
-            // Move the admin bar down and reset body margin
-            wpAdminBar.style.transform = 'translateY(0px)';
-            body.style.marginTop = '0px';
+function loadAccordions() {
+    chrome.storage.sync.get(['accordions'], function(result) {
+        if (result.accordions) {
+            result.accordions.forEach(accordionData => {
+                addAccordion(accordionData.title, accordionData.urls);
+            });
         }
+    });
+}
+
+function handleAddAccordion(event) {
+    event.preventDefault();
+    const accordionTitle = document.getElementById('accordionTitleInput').value.trim();
+    if (accordionTitle) {
+        addAccordion(accordionTitle);
+        document.getElementById('accordionTitleInput').value = '';
+        saveAccordions();
     }
 }
 
+function addAccordion(title, urls = []) {
+    const accordionContainer = document.getElementById('accordionContainer');
+    const accordion = createAccordion(title);
+    accordionContainer.prepend(accordion);
+
+    urls.forEach(url => addUrlToAccordion(accordion, url));
+    const urlForm = createUrlForm(accordion);
+    accordion.querySelector('.accordion-body').prepend(urlForm);
+}
+
+function createAccordion(title) {
+    const accordion = document.createElement('div');
+    accordion.classList.add('accordion');
+
+    const header = document.createElement('div');
+    header.classList.add('accordion-header');
+    header.innerText = title;
+    header.addEventListener('click', () => accordion.querySelector('.accordion-body').classList.toggle('active'));
+
+    const body = document.createElement('div');
+    body.classList.add('accordion-body');
+    const table = document.createElement('table');
+    body.appendChild(table);
+
+    accordion.appendChild(header);
+    accordion.appendChild(body);
+    return accordion;
+}
+
+function createUrlForm(accordion) {
+    const form = document.createElement('form');
+    form.classList.add('url-form');
+
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.placeholder = 'Enter a URL (with or without leading /)';
+    form.appendChild(input);
+
+    const submitButton = document.createElement('button');
+    submitButton.type = 'submit';
+    submitButton.innerText = 'Add URL';
+    form.appendChild(submitButton);
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const url = input.value.trim();
+        if (url) {
+            addUrlToAccordion(accordion, url);
+            input.value = '';
+            saveAccordions();
+        }
+    });
+
+    return form;
+}
+
+
+function addUrlToAccordion(accordion, url) {
+    const table = accordion.querySelector('table');
+    const row = document.createElement('tr');
+
+    const navigateCell = document.createElement('td');
+    const navigateButton = document.createElement('button');
+    navigateButton.innerText = 'Go';
+    navigateButton.addEventListener('click', () => navigateToUrl(url));
+    navigateCell.appendChild(navigateButton);
+    row.appendChild(navigateCell);
+
+    const urlCell = document.createElement('td');
+    urlCell.innerText = url;
+    row.appendChild(urlCell);
+
+    const editCell = document.createElement('td');
+    const editButton = document.createElement('button');
+    editButton.innerText = 'Edit';
+    editButton.addEventListener('click', () => {
+        const newUrl = prompt('Edit URL', url).trim();
+        if (newUrl) {
+            urlCell.innerText = newUrl;
+            saveAccordions();
+        }
+    });
+    editCell.appendChild(editButton);
+    row.appendChild(editCell);
+
+    const deleteCell = document.createElement('td');
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Delete';
+    deleteButton.addEventListener('click', () => {
+        if (confirm('Are you sure you want to delete this URL?')) {
+            row.remove();
+            saveAccordions();
+        }
+    });
+    deleteCell.appendChild(deleteButton);
+    row.appendChild(deleteCell);
+
+    table.appendChild(row);
+}
+
+function navigateToUrl(relativeUrl) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        if (!tabs[0] || !tabs[0].url) return;
+
+        const currentTabUrl = new URL(tabs[0].url);
+        const fullPath = relativeUrl.startsWith('/') ? relativeUrl : '/' + relativeUrl;
+        const fullUrl = currentTabUrl.origin + fullPath;
+        chrome.tabs.update(tabs[0].id, { url: fullUrl });
+    });
+}
+
+
+// Ensure this function is called after adding a new URL to an accordion
+function addEventListenersToButtons() {
+    document.querySelectorAll('.accordion .go-button').forEach(button => {
+        button.removeEventListener('click', onGoButtonClick); // Remove existing listener to avoid duplicates
+        button.addEventListener('click', onGoButtonClick);
+    });
+}
+
+function onGoButtonClick(event) {
+    const url = event.target.getAttribute('data-url');
+    if (url) {
+        navigateToUrl(url);
+    }
+}
+
+// Call addEventListenersToButtons() after adding new URLs or creating new accordions
+
+
+function saveAccordions() {
+    const accordions = [];
+    document.querySelectorAll('.accordion').forEach(accordion => {
+        const title = accordion.querySelector('.accordion-header').innerText;
+        const urls = Array.from(accordion.querySelectorAll('td:nth-child(2)'))
+                          .map(td => td.innerText);
+        accordions.push({ title, urls });
+    });
+    chrome.storage.sync.set({ 'accordions': accordions });
+}
